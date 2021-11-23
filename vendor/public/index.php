8ebf0173a252b9c5f720649636289948dd64c64b
“13799180807” <1397165474@qq.com>
2021-11-23 18:02:35 +0800

    11

--- vendor/public/index.php
+++ vendor/public/index.php
@@ -9,7 +9,30 @@ $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
+
     return $response;
 });
+$app->get('/1', function (Request $request, Response $response, $args) {
+ //  var_dump( $request);
+    $response->getBody()->write("我是新建的的1在路由上可以找到我哦!");
+
+    var_dump($args);
+    return $response;
+});
+$app->get('/2', function (Request $request, Response $response, $args) {
+
+
+
+
+     include "1.php";
+    //$response->getBody()->write("我是新建的的2!");
+     return $response;
+});
+
+$app->get('', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("404");
+    return $response;
+});
+
 
 $app->run();
